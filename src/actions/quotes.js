02390f51114ef1cf619f58fa7fb5c3b78eb3888d
Joy Lin <vvlnote@hotmail.com>
2019-09-11 22:08:59 +0000

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,6 +1,7 @@
 // TODO: Create action creators as defined in tests
+
 // export function addQuote(quote= {}) {
-//   return {type: "ADD_QUOTE", 
+//   return {type: "ADD_QUOTE",
 //   quote: Object.assign({}, quote, {votes:0})};
 // }
 
--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -7,7 +7,6 @@ class QuoteForm extends Component {
 
   state = {
     content: '',
-    author: '',
   }
 
   handleOnChange = event => {
