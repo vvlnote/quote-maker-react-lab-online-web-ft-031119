b62ed5a2b7cd506936113b5d080f4f9f4bea4236
Joy Lin <vvlnote@hotmail.com>
2019-09-11 22:04:34 +0000

    Automatically backed up by Learn

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,7 +1,7 @@
 // TODO: Create action creators as defined in tests
-<<<<<<< HEAD
+
 // export function addQuote(quote= {}) {
-//   return {type: "ADD_QUOTE", 
+//   return {type: "ADD_QUOTE",
 //   quote: Object.assign({}, quote, {votes:0})};
 // }
 
@@ -18,24 +18,5 @@ export const upvoteQuote = quoteId => {
 }
 
 export const downvoteQuote = quoteId => {
-=======
-export function addQuote(quote= {}) {
-  return {type: "ADD_QUOTE", quote: quote};
-}
-
-// export const addQuote = quote => {
-//   return {type: "ADD_QUOTE", quote: quote};
-// }
-
-export function removeQuote(quoteId) {
-  return {type: "REMOVE_QUOTE", quoteId: quoteId};
-}
-
-export function upvoteQuote(quoteId) {
-  return {type: "UPVOTE_QUOTE", quoteId: quoteId};
-}
-
-export function downvoteQuote(quoteId) {
->>>>>>> f8e88917e1d650d06d376331c7ad89e440911536
   return {type: "DOWNVOTE_QUOTE", quoteId: quoteId};
 }
